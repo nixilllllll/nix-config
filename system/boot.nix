ba971b0793cf2413eb0f0d063cf65dad17cb3cfb
nixilllllll <haryga2003@gmail.com>
2026-08-28 15:25:04 +0300

    chore(boot): switch systemd-boot to grub

--- system/boot.nix
+++ system/boot.nix
@@ -2,8 +2,14 @@
 
 {
   # --- BOOTLOADER ---
-  boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
+
+  boot.loader.grub = {
+    enable = true;
+    device = "nodev";
+    efiSupport = true;
+    useOSProber = true;
+  };
 
   boot.plymouth = {
     enable = true;
