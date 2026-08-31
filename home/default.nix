b3398f285fcfc89d5704815901728ad0b31f75bb
nixilllllll <haryga2003@gmail.com>
2026-09-01 02:54:54 +0300

    fix(desktop): fix KDE session initialisation by removing unused qt/gtk themes

--- home/default.nix
+++ home/default.nix
@@ -9,29 +9,4 @@
   home.username = "xbscure";
   home.homeDirectory = "/home/xbscure";
   home.stateVersion = "26.05";
-
-  # --- ТЕМЫ И ИКОНКИ PAPIRUS ---
-  gtk = {
-    enable = true;
-    iconTheme = {
-      name = "Papirus-Dark";
-      package = pkgs.papirus-icon-theme;
-    };
-    cursorTheme = {
-      name = "Bibata-Modern-Ice";
-      package = pkgs.bibata-cursors;
-      size = 24;
-    };
-  };
-
-  qt = {
-    enable = true;
-    platformTheme.name = "gtk";
-    style.name = "adwaita-dark";
-  };
-
-  # Переменные окружения пользователя
-  home.sessionVariables = {
-    EDITOR = "zeditor";
-  };
 }
