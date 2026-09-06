ef8c9483eae52065fbc06e968582a07f4240dbe9
nixilllllll <haryga2003@gmail.com>
2026-09-06 17:53:29 +0300

    fix(flake): pin home-manager input to release-26.05 branch

--- flake.nix
+++ flake.nix
@@ -6,7 +6,7 @@
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
 
     home-manager = {
-      url = "github:nix-community/home-manager";
+      url = "github:nix-community/home-manager/release-26.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
